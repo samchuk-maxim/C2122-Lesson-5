8c3675226ae3fbb712a946abfa939e6b3dbea4d5
samchuk-maxim <mk2b79@gmail.com>
2024-10-26 17:52:08 +0300

    add validation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from contextlib import nullcontext
-
 print("Lesson 5")
 
 class Employee:
@@ -22,10 +20,10 @@ class Employee:
     def to_receive_a_salary(self):
         print("Ваша ЗП:", self.Salary)
 
-emloyee1=Employee("Sigma","Oleksand",37,7)
-
-emloyee1.print_employee_info()
-emloyee1.to_receive_a_salary()
+# emloyee1=Employee("Sigma","Oleksand",37,7)
+#
+# emloyee1.print_employee_info()
+# emloyee1.to_receive_a_salary()
 
 
 class Driver(Employee):
@@ -37,13 +35,13 @@ class Driver(Employee):
     def show_transport(self):
         print("Транспорт:", self.Transport)
 
-print()
+# print()
 
-emloyee2=Driver("Kogut","Oleg",27,2,"AboBus Bohdan")
-
-emloyee2.print_employee_info()
-emloyee2.show_transport()
-emloyee2.to_receive_a_salary()
+# emloyee2=Driver("Kogut","Oleg",27,2,"AboBus Bohdan")
+#
+# emloyee2.print_employee_info()
+# emloyee2.show_transport()
+# emloyee2.to_receive_a_salary()
 
 class Inspector(Employee):
     def __init__(self,surname, name, age, experience,license):
@@ -54,11 +52,44 @@ class Inspector(Employee):
     def show_license(self):
         print("Номер ліцензії:", self.License)
 
-print()
+# print()
 
-emloyee3=Inspector("Kogut","Oleg",27,2,"123443-123123")
+# try:
+#     emloyee3=Inspector("Kogut","Oleg",27,2,"123443-123123")
+#     emloyee3.print_employee_info()
+#     emloyee3.show_license()
+#     emloyee3.to_receive_a_salary()
+# except TypeError as error:
+#     print(f"Error: {error}")
 
-emloyee3.print_employee_info()
-emloyee3.show_license()
-emloyee3.to_receive_a_salary()
 
+try:
+    print("Ведіть прізвеще:")
+    surname=input()
+    if type(surname)!=str:
+        raise TypeError("Surname is not string")
+
+    print("Ведіть імя:")
+    name = input()
+    if type(name)!=str:
+        raise TypeError("Name is not string")
+
+    print("Ведіть вік:")
+    age = int(input())
+    if type(age) != int:
+        raise TypeError("Age is not int")
+    if 18 <= age > 140:
+        raise Exception("Неможливий вік")
+
+    print("Ведіть досвід:")
+    experience = int(input())
+    if type(experience)!=int:
+        raise TypeError("Experience is not int")
+
+    print("Ведіть авто:")
+    transport = input()
+    if type(transport) != str:
+        raise TypeError("Transport is not string")
+
+except Exception as error:
+    print(f"Error text:{error}")
